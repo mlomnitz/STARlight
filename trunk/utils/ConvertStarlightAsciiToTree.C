cb477f167056dc35c142d6f98e76f832168b6a6c
Michael Lomnitz <mrlomnitz@lbl.gov>
2017-09-05 12:19:06 -0700

    Include virtual photon and scattered electron when converting output to root tree

--- trunk/utils/ConvertStarlightAsciiToTree.C
+++ trunk/utils/ConvertStarlightAsciiToTree.C
@@ -27,6 +27,15 @@
 using namespace std;
 double IDtoMass(int particleCode);
 
+struct source_electron
+{
+  TLorentzVector* p;
+  float gamma_mass;
+  void set(float px, float py, float pz, float E, float Q2){
+    p = new TLorentzVector(px,py,pz,E);
+    gamma_mass = Q2;
+  };
+};
 
 void ConvertStarlightAsciiToTree(const char* inFileName  = "slight.out",
                         const char* outFileName = "starlight.root")
@@ -41,12 +50,19 @@ void ConvertStarlightAsciiToTree(const char* inFileName  = "slight.out",
 	TTree*          outTree           = new TTree("starlightTree", "starlightTree");
 	TLorentzVector* parentParticle    = new TLorentzVector();
   	TClonesArray*   daughterParticles = new TClonesArray("TLorentzVector");
+	TLorentzVector* source            = new TLorentzVector();
+	Float_t        q2, Egamma;
+
+	outTree->Branch("Egamma",         &Egamma, "Egamma/F");
+	outTree->Branch("Q2",         &q2, "q2/F");
+	outTree->Branch("source",    "TLorentzVector", &source,            32000, -1);
 	outTree->Branch("parent",    "TLorentzVector", &parentParticle,    32000, -1);
 	outTree->Branch("daughters", "TClonesArray",   &daughterParticles, 32000, -1);
 
 	ifstream inFile;
 	inFile.open(inFileName);
 	unsigned int countLines = 0;
+	int tot_events=0;
 	while (inFile.good()) {
 		string       line;
 		stringstream lineStream;
@@ -57,11 +73,14 @@ void ConvertStarlightAsciiToTree(const char* inFileName  = "slight.out",
 		if (!getline(inFile, line))
 			break;
 		++countLines;
+		++tot_events;
 		lineStream.str(line);
 		assert(lineStream >> label >> eventNmb >> nmbTracks);
 		if (!(label == "EVENT:"))
 			continue;
 		
+		lineStream.clear();
+
 		// read vertex
 		if (!getline(inFile, line))
 			break;
@@ -69,14 +88,39 @@ void ConvertStarlightAsciiToTree(const char* inFileName  = "slight.out",
 		lineStream.str(line);
 		assert(lineStream >> label);
 		assert(label == "VERTEX:");
-			
+		
 		*parentParticle = TLorentzVector(0, 0, 0, 0);
+
+		lineStream.clear();		
+		
+		//read gamma
+		if( !getline(inFile,line))
+		  break;
+		lineStream.str(line);
+		//cout<<line.c_str()<<endl;
+		assert(lineStream >> label >> Egamma >> q2 );
+		assert(label == "GAMMA:");
+		lineStream.clear();
+ 
+		// read source
+		if(!getline(inFile, line))
+		  break;
+		++countLines;
+		lineStream.str(line);
+		//cout<<line.c_str()<<endl;
+		float px=0., py=0., pz=0., E=0.;
+		assert(lineStream >> label >> px >> py >> pz >> E) ;
+		assert(label == "SOURCE:");
+		*source = TLorentzVector(px, py, pz, E);
+
+		lineStream.clear();
+		//
 		for (int i = 0; i < nmbTracks; ++i) {
 			// read tracks
 			int    particleCode;
 			double momentum[3];
 			if (!getline(inFile, line))
-				break;
+			  break;
 			++countLines;
 			lineStream.str(line);
 			assert(lineStream >> label >> particleCode >> momentum[0] >> momentum[1] >> momentum[2]);
@@ -87,16 +131,18 @@ void ConvertStarlightAsciiToTree(const char* inFileName  = "slight.out",
 			                      + momentum[2] * momentum[2] + daughterMass * daughterMass);
 			new ( (*daughterParticles)[i] ) TLorentzVector(momentum[0], momentum[1], momentum[2], E);
 			*parentParticle += *(static_cast<TLorentzVector*>(daughterParticles->At(i)));
+
+			lineStream.clear();
 		}
 		daughterParticles->Compress();
 		outTree->Fill();
 	}
-
 	outTree->Write();
 	if (outFile) {
 		outFile->Close();
 		delete outFile;
 	}
+	cout<<"Processed "<<tot_events<<" events"<<endl;
 }
 
 	double IDtoMass(int particleCode){
